ee5d65da1d205f292a961ed58596b6c76803942a
prithasathish <thayaprith10@gmail.com>
2025-10-24 22:39:37 +0530

    Changes

--- server.js
+++ server.js
@@ -26,6 +26,29 @@ app.post("/api/register", async (req, res) => {
   }
 });
 
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Validate password
+    if (user.password !== password) {
+      return res.status(400).json({ error: "Invalid Password" });
+    }
+
+    res.status(200).json({ message: "Login Successful", user });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
 app.listen(5000, () => {
   console.log("🚀 Server is running on port 5000");
 });
